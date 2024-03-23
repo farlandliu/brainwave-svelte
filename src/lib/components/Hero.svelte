490dbcc64ee4043008f3b9e406f39435c14e5c62
farland <farland@163.com>
2024-03-23 12:12:30 +0000

    sync

--- src/lib/components/Hero.svelte
+++ src/lib/components/Hero.svelte
@@ -4,6 +4,21 @@
 
 <section class="-mt-[5.25rem] pt-[12rem]" id="hero">
 	<div class="container relative">
-		hero
+		<div
+			class="relative z-1 max-w-[62rem] mx-auto text-center mb-[3.875rem] md:mb-20 lg:mb-[6.25rem]"
+		>
+			<h1
+				class="mb-6 bg-gradient-to-r from-fuchsia-500 to-cyan-500 bg-clip-text text-6xl text-transparent"
+			>
+				Explore the Possibilities of&nbsp;AI&nbsp;Chatting
+			</h1>
+			<p class="body-1 max-w-3xl mx-auto mb-6 text-n-2 lg:mb-8">
+				Unleash the power of AI within Brainwave. Upgrade your productivity
+				with Brainwave, the open AI chat app.
+			  </p>
+			  <Button href="/pricing" white>
+				Get started
+			  </Button>
+		</div>
 	</div>
 </section>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,4 @@
-<h1 class="text-center text-3xl font-bold text-orange-200 underline">Hello world!</h1>
-
-<p class="text-center font-grotesk text-4xl">Sphinx of black quartz, judge my vow.</p>
+<script lang="ts">
+    import Hero from "$lib/components/Hero.svelte";
+</script>
+<Hero />
